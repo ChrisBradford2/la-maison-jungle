af8a8ceb795672cd7657c85c462522e6ada65df5
Nicolas Barbarisi <nico.bar2012@gmail.com>
2022-03-07 15:45:32 +0100

    Add Banner and Cart component

---
+++ src/component/App.js
@@ -0,0 +1,8 @@
+//import logo from './logo.svg';
+import Banner from './Banner'
+
+function App() {
+    return <Banner />
+}
+
+export default App
--- src/component/App.test.js
+++ src/component/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/La maison jungle/i);
   expect(linkElement).toBeInTheDocument();
 });
