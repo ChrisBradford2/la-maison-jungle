4020c87950ccb93ad061d36fe97ab2bc9171a65a
Chris <nico.bar2012@gmail.com>
2022-03-12 20:17:51 +0100

    EL-116 : Add ShoppingList component.

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import Banner from './Banner'
 import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
   return (
     <div>
       <Banner />
+      <ShoppingList />
       <Cart />
     </div>
   )
